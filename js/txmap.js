a3804a28fa5254bca90cbf18735edc325f5fa859
hygge666 <1691617546@qq.com>
2023-04-12 21:57:21 +0800

    Site updated: 2023-04-12 21:57:19

--- js/txmap.js
+++ js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(117.314729 , 29.32471, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(117.317048 , 29.334518, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
